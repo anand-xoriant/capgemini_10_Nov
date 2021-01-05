2d842d8eb55b82bea768fddd009c032cfac66b21
Anand <anand.pune38@gmail.com>
2021-01-05 21:20:19 +0530

    React first app

---
+++ ReactJS/shared_code/first-react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { HelloComponent } from './components/hello';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello React App..</h1>
+     <HelloComponent></HelloComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/shared_code/first-react-app/src/components/hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class HelloComponent extends React.Component {
+    render() {
+        return <div>
+            <input type='text'></input>
+            <br></br>
+            <br></br>
+            <input type='submit' value='SUBMIT'></input>
+        </div>
+    }
+}
